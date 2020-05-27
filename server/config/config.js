39e021ea12891122caa25abebe523c2f3e47a328
U-DESKTOP-6PMJ63I\User <eduardoberzunzal@gmail.com>
2020-05-27 18:50:00 -0500

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
 	urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-	urlDB = 'mongodb+srv://SirEduars:oxzmBwxPHViCeyO6@cluster0-txcij.mongodb.net/cafe';
+	urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
